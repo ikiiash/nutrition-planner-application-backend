174b342729b176b0c9ac48af39f477be499932de
ivan.kiiashchenko <ivan.kiiashchenko@student.tuke.sk>
2026-04-08 12:49:59 +0200

    chore(config): setup DB schema, JPA mapping and application config

--- application/springboot/src/main/java/sk/posam/fsa/nutritionplanner/NutritionPlannerApplication.java
+++ application/springboot/src/main/java/sk/posam/fsa/nutritionplanner/NutritionPlannerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NutritionPlannerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(NutritionPlannerApplication.class, args);
     }
 }
